5e50ee7d6ee507a182f80a9c3fb5be1f243ac052
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2022-12-17 14:28:57 -0300

    InsertionShort

---
+++ InsertionShort Algorithm/InsertionShort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void insertionShort(int arrranjo[], int tamArranjo){  // Algoritmo de ordenação InsertionShort
+    int chave, i = 0, j = 0;
+
+    for (j = 1; j < tamArranjo; j++){
+        chave = arrranjo[j];  
+        i = j - 1;
+
+        while((i >= 0) && (arrranjo[i] > chave)){
+            arrranjo[i + 1] = arrranjo[i];
+            i = i - 1;
+        }
+        arrranjo[i + 1] = chave;
+    }
+}
+
+
+int main(){
+    int arranjo[] = {1,2,3,4,1,6,0, -1, 100, 7}; // Arranjo exemplo
+
+    int tamArranjo = sizeof(arranjo) / sizeof(arranjo[0]);  // Nassa expressão eu pego o tamanho em bytes do arranjo e divido ele pelo tamanho do tipo do primeiro intem do arranjo, assim achando a quantidade de intens do arranjo.
+
+    insertionShort(arranjo, tamArranjo);  // Chamando a função e passando os valores 
+
+    for(int i =0 ; i < tamArranjo; i++){  // Printado arranjo ordenado
+        printf("%d ", arranjo[i]);
+    }
+
+    return 0;
+}
